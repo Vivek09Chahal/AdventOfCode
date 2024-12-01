44df0f5195f41e24e4f4c42b5ec7839642d276f0
Vivek <103368320+Vivek09Chahal@users.noreply.github.com>
2024-12-01 11:47:39 +0530

    Updated 1st Solution combined both part

--- 1st.cpp
+++ 1st.cpp
@@ -2,15 +2,45 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
+#include <map>
 
-int main() {
+int part1(std::vector<int> &leftList, std::vector<int> &rightList)
+{
+    int totalDistance = 0;
+    for (size_t i = 0; i < leftList.size(); ++i)
+    {
+        totalDistance += std::abs(leftList[i] - rightList[i]);
+    }
+
+    return totalDistance;
+}
+
+int part2(std::vector<int> &leftList, std::vector<int> &rightList)
+{
+    std::map<int, int> countMap;
+    for (int num : rightList)
+    {
+        countMap[num]++;
+    }
+
+    // Calculate the similarity score
+    int similarityScore = 0;
+    for (int num : leftList)
+    {
+        similarityScore += num * countMap[num];
+    }
+    return similarityScore;
+}
+
+int main()
+{
     std::ifstream infile("1st.txt");
     std::vector<int> leftList, rightList;
     int left, right;
 
     // Read pairs from the file and populate the lists
-    while (infile >> left >> right) {
+    while (infile >> left >> right)
+    {
         leftList.push_back(left);
         rightList.push_back(right);
     }
@@ -19,14 +49,11 @@ int main() {
     std::sort(leftList.begin(), leftList.end());
     std::sort(rightList.begin(), rightList.end());
 
-    // Calculate the total distance
-    int totalDistance = 0;
-    for (size_t i = 0; i < leftList.size(); ++i) {
-        totalDistance += std::abs(leftList[i] - rightList[i]);
-    }
+    int answer1 = part1(leftList, rightList);
+    std::cout << "Total Distance: " << answer1 << std::endl;
 
-    // Output the total distance
-    std::cout << "Total Distance: " << totalDistance << std::endl;
+    int answer2 = part2(leftList, rightList);
+    std::cout << "Similarity Score: " << answer2 << std::endl;
 
     return 0;
 }
